44b073ae921e1a425f7fabd4104cb434f8c50690
emfalkne <emfalkne@asu.edu>
2015-10-01 22:16:37 -0700

    added hearder guards

--- q.h
+++ q.h
@@ -9,7 +9,10 @@
 #include<iostream>
 #include<cstdio>
 #include<cstdlib>
-using namespace std;
+
+//START HEADER GUARD
+#ifndef Q_H
+#define Q_H
 
 //DATA STRUCTURES
 typedef struct qele //Create a struct of type qele
@@ -25,4 +28,7 @@ void InitQueue(qele *head);//creates an empty queue, pointed to by the variable 
 void AddQueue(qele *head, qele *item);//adds a queue item, pointed to by "item", to the queue pointed to by head
 qele *DelQueue(qele *head); //deletes an item from head and returns a pointer to the deleted item
 void RotateQ(qele *head);//moves the head pointer to the next element in the queue
-//void FreeItem(q-ele *item);//frees an item from memory
+void FreeItem(qele *item);//frees an item from memory
+
+//END HEADER GUARD
+#endif
