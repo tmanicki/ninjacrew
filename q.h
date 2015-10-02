f65b189e10c0ea5fc839d217d898c03dca3678ac
emfalkne <emfalkne@asu.edu>
2015-10-01 22:12:13 -0700

    re-named to qele

--- q.h
+++ q.h
@@ -12,17 +12,17 @@
 using namespace std;
 
 //DATA STRUCTURES
-typedef struct q-ele //Create a struct of type q-ele
+typedef struct qele //Create a struct of type qele
 {
 	int payload;
-	q-ele *prev;
-	q-ele *next;
+	qele *prev;
+	qele *next;
 };
 
 //FUNCTION DECLARATIONS
-q-ele *NewItem(); //returns a pointer to a new q-element
-void InitQueue(q-ele *head);//creates an empty queue, pointed to by the variable head
-void AddQueue(q-ele *head, q-ele *item);//adds a queue item, pointed to by "item", to the queue pointed to by head
-q-ele *DelQueue(q-ele *head); //deletes an item from head and returns a pointer to the deleted item
-void RotateQ(q-ele *head);//moves the head pointer to the next element in the queue
+qele *NewItem(); //returns a pointer to a new q-element
+void InitQueue(qele *head);//creates an empty queue, pointed to by the variable head
+void AddQueue(qele *head, qele *item);//adds a queue item, pointed to by "item", to the queue pointed to by head
+qele *DelQueue(qele *head); //deletes an item from head and returns a pointer to the deleted item
+void RotateQ(qele *head);//moves the head pointer to the next element in the queue
 //void FreeItem(q-ele *item);//frees an item from memory
