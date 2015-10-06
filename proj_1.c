e9fb9b85b8e4f525d112394d6fc67e00181ea5e1
tmanicki <tmanicki@asu.edu>
2015-10-06 13:26:55 -0700

    transfer to C
    I kept the c++ file for reference, but the C file has been updated and
    from hence forth will be used

--- proj_1.c
+++ proj_1.c
@@ -1,35 +1,62 @@
-
 /**********************************************************************************
 **Authors: Emily Falkner, Tim Manicki
 **Class: CSE 430: Operating Systems
 **Professor: Partha Dasgupta
 **Assignment: Project 1
-**File Name: proj_1.c
+**File Name: proj_1.cpp
 ***********************************************************************************/
-
+//HEADER FILES
 #include "q.h"
+#include<stdio.h>
+qele *head= null;
+qele *current = head;
 
-
-using namespace std;
 
 int main() 
-{
-	
+
 	
 	return 0;	
 }
 
-q-ele *NewItem ()
+NewItem ()
 {
 	
+//	return current->next;
 	
 }
 
-InitQueue ()
+InitQueue (&head)
 {
 	
 }
 
+AddQueue(&head, item) 
+{
+	qele *temp;
+	temp = (qele*) malloc (sizeof (qele));
+	
+	
+	
+	else { return current-> next; }
+}
 
+DelQueue(&head)
+{
+	
+}
 
+// Deletes header
+RotateQ (&head)
+{
+	qele *temp;
+	temp = (qele*) malloc (sizeof (qele));
+	temp = header;
+	header = temp->next;
+	header->prev = NULL;
+	FreeItem (temp);
+}
 
+FreeItem (item)
+{
+	free item;
+}
