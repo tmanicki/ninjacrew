577e94bd35cd606eb71cd833e14c188a6bfe9868
emfalkne <emfalkne@asu.edu>
2015-10-13 22:32:32 -0700

    added return

--- tcb.h
+++ tcb.h
@@ -27,6 +27,8 @@ void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size)
     tcb->context.uc_stack.ss_sp = stackP;
     tcb->context.uc_stack.ss_size = (size_t) stack_size;
     makecontext(&tcb->context, function, 0);// context is now cooked
+	
+	return;
 }
 
 #endif
