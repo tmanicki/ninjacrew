ce04959615f36be86dc22e4f3e41020c3dc435c6
emfalkne <emfalkne@asu.edu>
2015-10-13 18:44:58 -0700

    made some changes

--- q.h
+++ q.h
@@ -15,20 +15,28 @@
 #define Q_H
 
 //DATA STRUCTURES
-typedef struct qele //Create a struct of type qele
+typedef struct q //Create a struct of type qele
 {
 	int payload;
 	qele *prev;
 	qele *next;
-};
+}qele;
+
 
 //FUNCTION DECLARATIONS
-qele *NewItem(); //returns a pointer to a new q-element
-void InitQueue(qele *head);//creates an empty queue, pointed to by the variable head
-void AddQueue(qele *head, qele *item);//adds a queue item, pointed to by "item", to the queue pointed to by head
-qele *DelQueue(qele *head); //deletes an item from head and returns a pointer to the deleted item
-void RotateQ(qele *head);//moves the head pointer to the next element in the queue
-void FreeItem(qele *item);//frees an item from memory
+struct qele* NewItem(); //returns a pointer to a new q-element
+void InitQueue(struct qele* head);//creates an empty queue, pointed to by the variable head
+void AddQueue(struct qele* head, struct qele* item);//adds a queue item, pointed to by "item", to the queue pointed to by head
+struct qele*  DelQueue(struct qele *head)
+{
+	//checks if the queue is empty
+	
+}
+
+void RotateQ(struct qele *head)
+{
+	*head = (*head)-> next; //Moves the head pointer to the next element in the queue
+}
 
 //END HEADER GUARD
 #endif
